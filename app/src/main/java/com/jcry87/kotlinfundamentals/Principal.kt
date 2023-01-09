7952ef061bf4931dd0eadd562f67cb7049473088
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-01-09 14:15:26 -0500

    Usage of variables and functions

--- app/src/main/java/com/jcry87/kotlinfundamentals/Principal.kt
+++ app/src/main/java/com/jcry87/kotlinfundamentals/Principal.kt
@@ -1,5 +1,18 @@
 package com.jcry87.kotlinfundamentals
 
 fun main() {
-    print("Hello Kotlin!!")
+    newTopic("Hello Kotlin")
+
+    val a = 2 // constant
+    val text = "Hello"
+    var b: Int = 3 // variable
+    var any: Any // cualquier tipo
+    var objNull: String? // allow null
+    objNull = null
+    println(a)
+    println(b)
+}
+
+fun newTopic(topic: String){
+    println(topic)
 }
